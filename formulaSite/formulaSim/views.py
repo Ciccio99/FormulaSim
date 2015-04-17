e1060db74b3219d32c30ff390622f38bfa9a80be
D Jayme Green <Djaymegreen@gmail.com>
2015-04-17 19:55:16 -0400

    Added basic Views and html files corresponding

--- formulaSite/formulaSim/views.py
+++ formulaSite/formulaSim/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import *
 
 # Create your views here.
+
+"""
+Three main pages of the web application
+Home: home page of what the application is
+About: How we do the database and the simulation,
+       would be very similar to paper
+RaceSim: Where the application actually queries the 
+          database and shows the simulation
+"""
+def home(request):
+	return render(request, 'home.html')
+
+def about(request):
+	return render(request, 'about.html')
+
+def raceSim(request):
+	return render(request, 'raceSim.html')
+
+"""
+Functions for simulation:
+"""
